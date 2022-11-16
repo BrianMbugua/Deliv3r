fcaffee2692cfaa2e36b488f1f5cc51691cfc6c2
Brian Mbugua <brianmbugua505@gmail.com>
2022-11-16 11:14:51 +0300

    Confirm password validation, email validation

--- flaskr/deliverer/forms.py
+++ flaskr/deliverer/forms.py
@@ -1,11 +1,17 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField, BooleanField, SelectField, TextAreaField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError 
+from flaskr.models import Deliverer
 
 class RegistrationForm(FlaskForm):
-    user_name = StringField('First Name', validators=[DataRequired(), Length(min=2, max=10)])
-    user_name = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=10)])
+    firstname = StringField('First Name', validators=[DataRequired(), Length(min=2, max=10)])
+    lastname = StringField('Last Name', validators=[DataRequired(), Length(min=2, max=10)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired()])   
-    confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    confirmpassword = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
     submit = SubmitField('Sign Up')
+
+    def validate_email(self, email):
+        deliverer = Deliverer.query.filter_by(email=email.data).first()
+        if deliverer:
+            raise ValidationError('Email is already linked to another account')
--- flaskr/deliverer/routes.py
+++ flaskr/deliverer/routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, request, redirect
+from flask import Blueprint, render_template, request, redirect, flash
 from flaskr import bcrypt, db
 from flaskr.deliverer.forms import RegistrationForm
 from flaskr.models import Deliverer
@@ -11,19 +11,17 @@ deliverer = Blueprint('deliverer', __name__, url_prefix='/deliverer', static_fol
 def register():
     form = RegistrationForm()
 
-    if form.is_submitted():
-        form_data = request.form
-        hashed_password = bcrypt.generate_password_hash(form_data["password"]).decode('utf-8')
+    if form.validate_on_submit():
+        hashed_password = bcrypt.generate_password_hash(form.password.data).decode('utf-8')
         
+        deliverer = Deliverer(
+            firstname=form.firstname.data, lastname=form.lastname.data, email=form.email.data, password=hashed_password)
 
-        deliverer = Deliverer.query.filter_by(email=form_data["password"]).first()
-        print(deliverer)
-        if deliverer is None:
-            deliverer = Deliverer(firstname=form_data["firstname"], lastname=form_data["lastname"], email=form_data["email"], password=hashed_password)
-            db.session.add(deliverer)
-            db.session.commit()
-
-        return redirect(request.url)
+        db.session.add(deliverer)
+        db.session.commit()
+        flash('Your Account has been created! You can now login', 'success')
+    else:
+        return render_template("deliverer_register.html", form=form)
 
 
     return render_template("deliverer_register.html", form=form)
